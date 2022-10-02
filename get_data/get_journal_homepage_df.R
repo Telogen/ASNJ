dfb8c73f3dd8dfea4d48d06827c0082af1e692b6
Lejin Tian <ljtian20@fudan.edu.cn>
2022-10-03 00:18:36 +0800

    20221002

--- get_data/get_journal_homepage_df.R
+++ get_data/get_journal_homepage_df.R
@@ -1,11 +1,16 @@
 journal_homepage_df <-
-  data.frame(journal = c('Phenomics','STTT','Cell Discovery','Cell Research'),
+  data.frame(journal = c('Phenomics',
+                         'STTT',
+                         'Cell Discovery',
+                         'Cell Research',
+                         'CMI'),
              homepage = c('https://www.springer.com/journal/43657',
                           'https://www.nature.com/sigtrans/',
                           'https://www.nature.com/celldisc/',
-                          'https://www.nature.com/cr/'))
+                          'https://www.nature.com/cr/',
+                          'https://www.nature.com/cmi/'))
 
-usethis::use_data(journal_homepage_df)
+usethis::use_data(journal_homepage_df,overwrite = TRUE)
 
 
 
